fe525876bb5c5d3da095e8c515c9da40437c0ba2
YonePC <YonePC@YONE>
2018-01-21 16:14:47 +0000

    refactor update

--- lessons/00/demo.js
+++ lessons/00/demo.js
@@ -8,7 +8,6 @@ var loader = setLoader();
 var files = setUrl();
 
 
-
 loader
     .load(files)
     .then(function () {
@@ -33,7 +32,23 @@ loader
         window.console.log(error);
     });
 
+function displaySeriesInfo(series) {
+    var seriesIndex = 1;
+    for (var mySeries of series) {
+        var seriesDiv = document.createElement('div');
+        seriesDiv.className += 'indent';
+        seriesDiv.insertAdjacentHTML('beforeend', '<div> SERIES (' + seriesIndex + '/' + series.length + ')</div>');
+        seriesDiv.insertAdjacentHTML(
+            'beforeend',
+            '<div class="series"> numberOfChannels: ' + mySeries.numberOfChannels + '</div>'
+        );
 
+        container.appendChild(seriesDiv);
+
+        displayStackInfo(mySeries, seriesDiv);
+        seriesIndex++;
+    }
+}
 
 
 
