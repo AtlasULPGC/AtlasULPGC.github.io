2cfda84d5e24668ba88964ba6abc92543b703a1b
YonePC <YonePC@YONE>
2018-01-22 16:49:19 +0000

    refactor: delete comments in lesson 6.

--- lessons/06/loader.js
+++ lessons/06/loader.js
@@ -1,5 +1,3 @@
-
-
 function setLoader() {
     var file = 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/eun_brain/eun_uchar_8.nii.gz';
 
@@ -8,21 +6,16 @@ function setLoader() {
         var series = loader.data[0].mergeSeries(loader.data)[0];
         loader.free();
         loader = null;
-        // get first stack from series
         var stack = series.stack[0];
 
         vrHelper = new AMI.VolumeRenderingHelper(stack);
-        // scene
         scene.add(vrHelper);
 
-        // CREATE LUT
         setLUT();
 
-        // update camrea's and interactor's target
         var centerLPS = updateCamera(stack);
         updateControls(centerLPS);
 
-        // create GUI
         buildGUI();
 
         fileLoadedAndGuiReady = true;
