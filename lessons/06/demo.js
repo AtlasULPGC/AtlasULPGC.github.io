97559c9fcd2bb9066978879fe6e6c2159c48befb
YonePC <YonePC@YONE>
2018-01-22 16:13:13 +0000

    refactor: extract loader in lesson 6

--- lessons/06/demo.js
+++ lessons/06/demo.js
@@ -99,36 +99,6 @@ function init() {
 // init threeJS...
 init();
 
-var file = 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/eun_brain/eun_uchar_8.nii.gz';
 
-var loader = new AMI.VolumeLoader(threeD);
-loader.load(file).then(function () {
-    var series = loader.data[0].mergeSeries(loader.data)[0];
-    loader.free();
-    loader = null;
-    // get first stack from series
-    var stack = series.stack[0];
 
-    vrHelper = new AMI.VolumeRenderingHelper(stack);
-    // scene
-    scene.add(vrHelper);
-
-    // CREATE LUT
-    lut = new AMI.LutHelper('my-tf');
-    lut.luts = AMI.LutHelper.presetLuts();
-    lut.lutsO = AMI.LutHelper.presetLutsO();
-    // update related uniforms
-    vrHelper.uniforms.uTextureLUT.value = lut.texture;
-    vrHelper.uniforms.uLut.value = 1;
-
-    // update camrea's and interactor's target
-    var centerLPS = stack.worldCenter();
-    camera.lookAt(centerLPS.x, centerLPS.y, centerLPS.z);
-    camera.updateProjectionMatrix();
-    controls.target.set(centerLPS.x, centerLPS.y, centerLPS.z);
-
-    // create GUI
-    buildGUI();
-
-    ready = true;
-});
+setLoader();
