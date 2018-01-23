620d7aabe8cfac307c650abeba2ddc9e04abb46d
YonePC <YonePC@YONE>
2018-01-23 18:57:59 +0000

    refactor EXTRACT setCameraOnTarget

--- lessons/04/camera.js
+++ lessons/04/camera.js
@@ -15,3 +15,26 @@ function setCamera() {
     );
 }
 
+function setCameraOnTarget(ctGrayImagesStack) {
+//
+    // set camera
+    var worldbb = ctGrayImagesStack.worldBoundingBox();
+    var lpsDims = new THREE.Vector3(worldbb[1] - worldbb[0], worldbb[3] - worldbb[2], worldbb[5] - worldbb[4]);
+
+    // box: {halfDimensions, center}
+    var box = {
+        center: ctGrayImagesStack.worldCenter().clone(),
+        halfDimensions: new THREE.Vector3(lpsDims.x + 10, lpsDims.y + 10, lpsDims.z + 10)
+    };
+
+    // init and zoom
+    var canvas = {
+        width: threeD.clientWidth,
+        height: threeD.clientHeight,
+    };
+    camera.directions = [ctGrayImagesStack.xCosine, ctGrayImagesStack.yCosine, ctGrayImagesStack.zCosine];
+    camera.box = box;
+    camera.canvas = canvas;
+    camera.update();
+    camera.fitBox(2);
+}
