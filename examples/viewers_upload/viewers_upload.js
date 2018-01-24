0f3198376879bef0d7bb491eef89dbfc42e841d9
YonePC <YonePC@YONE>
2018-01-24 07:53:42 +0000

    refactor delete comments

--- examples/viewers_upload/viewers_upload.js
+++ examples/viewers_upload/viewers_upload.js
@@ -1,12 +1,9 @@
 /* globals dat*/
 
-
-import buildGUI from './gui';
-import handleSeries from './handleSeries';
 import readMultipleFiles from './loader';
 import setCamera from './camera';
 import setControls from './controls';
-// standard global variables
+
 let renderer;
 let threeD;
 [threeD, renderer] = setRenderer();
@@ -16,7 +13,6 @@ let scene;
 let lut;
 
 
-// probe
 let camUtils = {
     invertRows: false,
     invertColumns: false,
@@ -25,52 +21,30 @@ let camUtils = {
     convention: 'radio',
 };
 
-
-/**
- * Init the scene
- */
 function init() {
-    /**
-     * Animation loop
-     */
+
     function animate() {
-        // render
+
         controls.update();
         renderer.render(scene, camera);
 
-        // request new frame
         requestAnimationFrame(function () {
             animate();
         });
     }
 
-    // renderer
-
-
-    // scene
     scene = new THREE.Scene();
-    // camera
-
-
-    // controls
-
 
     animate();
 }
 
 
 window.onload = function () {
-    // hookup load button
+
     setLoadButtonWithFileInput();
 
-    // init threeJS...
     init();
-
-
-
-
-
-    // hook up file input listener
+    
     document.getElementById('filesinput')
         .addEventListener('change', function (event) {
             readMultipleFiles(event, scene, camera, lut, camUtils, controls)
