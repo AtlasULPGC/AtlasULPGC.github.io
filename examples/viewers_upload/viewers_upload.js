fe1217a326a66c1e6eb72f790373b0da49cfc7c9
YonePC <YonePC@YONE>
2018-01-24 06:40:35 +0000

    refactor EXTRACT updateLabels in viewers_uploader example

--- examples/viewers_upload/viewers_upload.js
+++ examples/viewers_upload/viewers_upload.js
@@ -74,21 +74,6 @@ window.onload = function () {
     // init threeJS...
     init();
 
-    function updateLabels(labels, modality) {
-        if (modality === 'CR' || modality === 'DX') return;
-
-        let top = document.getElementById('top');
-        top.innerHTML = labels[0];
-
-        let bottom = document.getElementById('bottom');
-        bottom.innerHTML = labels[1];
-
-        let right = document.getElementById('right');
-        right.innerHTML = labels[2];
-
-        let left = document.getElementById('left');
-        left.innerHTML = labels[3];
-    }
 
     function buildGUI(stackHelper) {
         let stack = stackHelper._stack;
