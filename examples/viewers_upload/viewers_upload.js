2f45496d2356032bfc1224354dd948685f9878c5
YonePC <YonePC@YONE>
2018-01-24 06:55:33 +0000

    refactor EXTRACT hookCallbacks in viewers_uploader example

--- examples/viewers_upload/viewers_upload.js
+++ examples/viewers_upload/viewers_upload.js
@@ -15,14 +15,6 @@ let scene;
 let lut;
 
 
-let ctrlDown = false;
-let drag = {
-    start: {
-        x: null,
-        y: null,
-    },
-};
-
 // probe
 let camUtils = {
     invertRows: false,
@@ -74,95 +66,6 @@ window.onload = function () {
     // init threeJS...
     init();
 
-    /**
-     * Connect all callbevent observesrs
-     */
-    function hookCallbacks(stackHelper) {
-        let stack = stackHelper._stack;
-        // hook up callbacks
-        controls.addEventListener('OnScroll', function (e) {
-            if (e.delta > 0) {
-                if (stackHelper.index >= stackHelper.orientationMaxIndex - 1) {
-                    return false;
-                }
-                stackHelper.index += 1;
-            } else {
-                if (stackHelper.index <= 0) {
-                    return false;
-                }
-                stackHelper.index -= 1;
-            }
-        });
-
-        /**
-         * On window resize callback
-         */
-        function onWindowResize() {
-            let threeD = document.getElementById('r3d');
-            camera.canvas = {
-                width: threeD.clientWidth,
-                height: threeD.clientHeight,
-            };
-            camera.fitBox(2);
-
-            renderer.setSize(threeD.clientWidth, threeD.clientHeight);
-
-            // update info to draw borders properly
-            stackHelper.slice.canvasWidth = threeD.clientWidth;
-            stackHelper.slice.canvasHeight = threeD.clientHeight;
-        }
-
-        window.addEventListener('resize', onWindowResize, false);
-        onWindowResize();
-
-        /**
-         * On key pressed callback
-         */
-        function onWindowKeyPressed(event) {
-            ctrlDown = event.ctrlKey;
-            if (!ctrlDown) {
-                drag.start.x = null;
-                drag.start.y = null;
-            }
-        }
-
-        document.addEventListener('keydown', onWindowKeyPressed, false);
-        document.addEventListener('keyup', onWindowKeyPressed, false);
-
-        /**
-         * On mouse move callback
-         */
-        function onMouseMove(event) {
-            if (ctrlDown) {
-                if (drag.start.x === null) {
-                    drag.start.x = event.clientX;
-                    drag.start.y = event.clientY;
-                }
-                let threshold = 15;
-
-                stackHelper.slice.intensityAuto = false;
-
-                let dynamicRange = stack.minMax[1] - stack.minMax[0];
-                dynamicRange /= threeD.clientWidth;
-
-                if (Math.abs(event.clientX - drag.start.x) > threshold) {
-                    // window width
-                    stackHelper.slice.windowWidth +=
-                        dynamicRange * (event.clientX - drag.start.x);
-                    drag.start.x = event.clientX;
-                }
-
-                if (Math.abs(event.clientY - drag.start.y) > threshold) {
-                    // window center
-                    stackHelper.slice.windowCenter -=
-                        dynamicRange * (event.clientY - drag.start.y);
-                    drag.start.y = event.clientY;
-                }
-            }
-        }
-
-        document.addEventListener('mousemove', onMouseMove);
-    }
 
     /**
      * Visulaize incoming data
@@ -212,7 +115,7 @@ window.onload = function () {
 
         updateLabels(camera.directionsLabel, stack.modality);
         buildGUI(stackHelper, lut, camUtils, camera);
-        hookCallbacks(stackHelper);
+        hookCallbacks(stackHelper, controls, camera);
     }
 
     let loader = new LoadersVolume(threeD);
