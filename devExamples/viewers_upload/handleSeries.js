0f826972280682bd77809a5902581f9c806a33ee
YonePC <YonePC@YONE>
2018-01-24 10:46:30 +0000

    refactor: handleSeries to explain steps made after load is successful, also moved in other examples to devExamples.

--- devExamples/viewers_upload/handleSeries.js
+++ devExamples/viewers_upload/handleSeries.js
@@ -6,6 +6,45 @@ function cleanLoaderAndProgressBar(loader) {
     loader = null;
 }
 
+function coordinatesToCenterCamera(stack) {
+    let worldbb = stack.worldBoundingBox();
+    const xAxisCenter = (worldbb[1] - worldbb[0]) / 2;
+    const yAxisCenter = (worldbb[3] - worldbb[2]) / 2;
+    const zAxisCenter = (worldbb[5] - worldbb[4]) / 2;
+    let lpsDims = new THREE.Vector3(
+        xAxisCenter,
+        yAxisCenter,
+        zAxisCenter
+    );
+    return lpsDims;
+}
+
+function setCameraLens(stack, lpsDims) {
+    let box = {
+        center: stack.worldCenter().clone(),
+        halfDimensions:
+            new THREE.Vector3(lpsDims.x + 10, lpsDims.y + 10, lpsDims.z + 10),
+    };
+    return box;
+}
+
+function setCanvas() {
+    let canvas = {
+        width: threeD.clientWidth,
+        height: threeD.clientHeight,
+    };
+    return canvas;
+}
+
+function setCameraAfterImagesLoad(camera, stack, box, canvas) {
+    camera.directions = [stack.xCosine, stack.yCosine, stack.zCosine];
+    camera.box = box;
+    camera.canvas = canvas;
+    camera.update();
+    camera.fitBox(2);
+    updateLabels(camera.directionsLabel, stack.modality);
+}
+
 /**
  * Visulaize incoming data
  */
@@ -22,36 +61,12 @@ export default function handleSeries(seriesContainer, loader, scene, camera, lut
 
     console.log(stackHelper.stack);
 
-    // set camera
-    let worldbb = stack.worldBoundingBox();
-    const xAxisCenter = (worldbb[1] - worldbb[0]) / 2;
-    const yAxisCenter = (worldbb[3] - worldbb[2]) / 2;
-    const zAxisCenter = (worldbb[5] - worldbb[4]) / 2;
-    let lpsDims = new THREE.Vector3(
-        xAxisCenter,
-        yAxisCenter,
-        zAxisCenter
-    );
 
-    // box: {halfDimensions, center}
-    let box = {
-        center: stack.worldCenter().clone(),
-        halfDimensions:
-            new THREE.Vector3(lpsDims.x + 10, lpsDims.y + 10, lpsDims.z + 10),
-    };
+    let lpsDims = coordinatesToCenterCamera(stack);
+    let box = setCameraLens(stack, lpsDims);
+    let canvas = setCanvas();
 
-    // init and zoom
-    let canvas = {
-        width: threeD.clientWidth,
-        height: threeD.clientHeight,
-    };
-
-    camera.directions = [stack.xCosine, stack.yCosine, stack.zCosine];
-    camera.box = box;
-    camera.canvas = canvas;
-    camera.update();
-    camera.fitBox(2);
-    updateLabels(camera.directionsLabel, stack.modality);
+    setCameraAfterImagesLoad(camera, stack, box, canvas);
     buildGUI(stackHelper, lut, camUtils, camera);
     hookCallbacks(stackHelper, controls, camera);
 }
