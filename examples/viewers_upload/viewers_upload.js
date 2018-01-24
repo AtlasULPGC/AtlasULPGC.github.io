f4731d956c89a3c61e537b93defb654fb27ebc81
YonePC <YonePC@YONE>
2018-01-24 07:07:09 +0000

    refactor EXTRACT handleSeries in viewers_uploader example

--- examples/viewers_upload/viewers_upload.js
+++ examples/viewers_upload/viewers_upload.js
@@ -1,8 +1,9 @@
 /* globals dat*/
 import CoreUtils from 'base/core/core.utils';
 import LoadersVolume from 'base/loaders/loaders.volume';
-import HelpersStack from 'base/helpers/helpers.stack';
+
 import buildGUI from './gui';
+import handleSeries from './handleSeries';
 import setCamera from './camera';
 import setControls from './controls';
 // standard global variables
@@ -58,7 +59,6 @@ function init() {
 }
 
 
-
 window.onload = function () {
     // hookup load button
     setLoadButtonWithFileInput();
@@ -66,57 +66,6 @@ window.onload = function () {
     // init threeJS...
     init();
 
-
-    /**
-     * Visulaize incoming data
-     */
-    function handleSeries(seriesContainer) {
-        // cleanup the loader and its progress bar
-        loader.free();
-        loader = null;
-        // prepare for slice visualization
-        // first stack of first series
-        let stack = seriesContainer[0].mergeSeries(seriesContainer)[0].stack[0];
-
-        let stackHelper = new HelpersStack(stack);
-        stackHelper.bbox.visible = false;
-        stackHelper.borderColor = '#2196F3';
-        stackHelper.border.visible = false;
-        scene.add(stackHelper);
-
-        console.log(stackHelper.stack);
-
-        // set camera
-        let worldbb = stack.worldBoundingBox();
-        let lpsDims = new THREE.Vector3(
-            (worldbb[1] - worldbb[0]) / 2,
-            (worldbb[3] - worldbb[2]) / 2,
-            (worldbb[5] - worldbb[4]) / 2
-        );
-
-        // box: {halfDimensions, center}
-        let box = {
-            center: stack.worldCenter().clone(),
-            halfDimensions:
-                new THREE.Vector3(lpsDims.x + 10, lpsDims.y + 10, lpsDims.z + 10),
-        };
-
-        // init and zoom
-        let canvas = {
-            width: threeD.clientWidth,
-            height: threeD.clientHeight,
-        };
-
-        camera.directions = [stack.xCosine, stack.yCosine, stack.zCosine];
-        camera.box = box;
-        camera.canvas = canvas;
-        camera.update();
-        camera.fitBox(2);
-
-        updateLabels(camera.directionsLabel, stack.modality);
-        buildGUI(stackHelper, lut, camUtils, camera);
-        hookCallbacks(stackHelper, controls, camera);
-    }
 
     let loader = new LoadersVolume(threeD);
     let seriesContainer = [];
@@ -250,7 +199,7 @@ window.onload = function () {
         Promise
             .all(loadSequenceContainer)
             .then(function () {
-                handleSeries(seriesContainer);
+                handleSeries(seriesContainer, loader, scene, camera, lut, camUtils, controls);
             })
             .catch(function (error) {
                 window.console.log('oops... something went wrong...');
