786eb309c33a76bc19bc0ad9a6c469d930b738d4
YonePC <YonePC@YONE>
2018-01-25 17:02:36 +0000

    refactor: extract checkOnWhatRendererHasBeenTriggeredTheEvent

---
+++ devExamples/viewers_quadview/events.js
@@ -0,0 +1,37 @@
+function checkOnWhatRendererHasBeenTriggeredTheEvent(event, renderer3d, axialRenderer, sagittalRenderer, coronalRenderer) {
+    const canvas = event.target.parentElement;
+    const id = event.target.id;
+    const mouse = {
+        x: ((event.clientX - canvas.offsetLeft) / canvas.clientWidth) * 2 - 1,
+        y: -((event.clientY - canvas.offsetTop) / canvas.clientHeight) * 2 + 1,
+    };
+    //
+    let camera = null;
+    let stackHelper = null;
+    let scene = null;
+    switch (id) {
+        case '0':
+            camera = renderer3d.camera;
+            stackHelper = axialRenderer.stackHelper;
+            scene = renderer3d.scene;
+            break;
+        case '1':
+            camera = axialRenderer.camera;
+            stackHelper = axialRenderer.stackHelper;
+            scene = axialRenderer.scene;
+            break;
+        case '2':
+            camera = sagittalRenderer.camera;
+            stackHelper = sagittalRenderer.stackHelper;
+            scene = sagittalRenderer.scene;
+            break;
+        case '3':
+            camera = coronalRenderer.camera;
+            stackHelper = coronalRenderer.stackHelper;
+            scene = coronalRenderer.scene;
+            break;
+    }
+    return {mouse, camera, stackHelper, scene};
+}
+
+export {checkOnWhatRendererHasBeenTriggeredTheEvent};
