1b5c6c75d6fff3524b1ca222d265ac9997b75484
YonePC <YonePC@YONE>
2018-01-26 09:19:05 +0000

    refactor: EXTRACT updateLabels methods to explain them.

--- devExamples/viewers_upload/label.js
+++ devExamples/viewers_upload/label.js
@@ -1,15 +1,31 @@
 function updateLabels(labels, modality) {
-    if (modality === 'CR' || modality === 'DX') return;
+    if (isImageFormatWithoutCoordinateInfo(modality)) return;
+    updateTopLabel(labels);
+    updateBottomLabel(labels);
+    updateRightLabel(labels);
+    updateLeftLabel(labels);
+}
 
+function isImageFormatWithoutCoordinateInfo(modality) {
+    return modality === 'CR' || modality === 'DX';
+}
+
+function updateTopLabel(labels) {
     let top = document.getElementById('top');
     top.innerHTML = labels[0];
+}
 
+function updateBottomLabel(labels) {
     let bottom = document.getElementById('bottom');
     bottom.innerHTML = labels[1];
+}
 
+function updateRightLabel(labels) {
     let right = document.getElementById('right');
     right.innerHTML = labels[2];
+}
 
+function updateLeftLabel(labels) {
     let left = document.getElementById('left');
     left.innerHTML = labels[3];
 }
