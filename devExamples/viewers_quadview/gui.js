f7eaf9b419bae0927d88baa1b0c5eaffe536c6c7
YonePC <YonePC@YONE>
2018-02-01 11:05:30 +0000

    refactor: RENAME folders

--- devExamples/viewers_quadview/gui.js
+++ devExamples/viewers_quadview/gui.js
@@ -7,27 +7,27 @@ function setGui(axialRenderer, sagittalRenderer, coronalRenderer) {
     customContainer.appendChild(gui.domElement);
 
     // Red
-    let stackFolder1 = gui.addFolder('Axial (Red)');
-    let axialChanged = stackFolder1.add(
+    let axialFolder = gui.addFolder('Axial (Red)');
+    let axialChanged = axialFolder.add(
         axialRenderer.stackHelper,
         'index', 0, axialRenderer.stackHelper.orientationMaxIndex).step(1).listen();
-    stackFolder1.add(
+    axialFolder.add(
         axialRenderer.stackHelper.slice, 'interpolation', 0, 1).step(1).listen();
 
     // Yellow
-    let stackFolder2 = gui.addFolder('Sagittal (yellow)');
-    let sagittalChanged = stackFolder2.add(
+    let sagittalFolder = gui.addFolder('Sagittal (yellow)');
+    let sagittalChanged = sagittalFolder.add(
         sagittalRenderer.stackHelper,
         'index', 0, sagittalRenderer.stackHelper.orientationMaxIndex).step(1).listen();
-    stackFolder2.add(
+    sagittalFolder.add(
         sagittalRenderer.stackHelper.slice, 'interpolation', 0, 1).step(1).listen();
 
     // Green
-    let stackFolder3 = gui.addFolder('Coronal (green)');
-    let coronalChanged = stackFolder3.add(
+    let coronalFolder = gui.addFolder('Coronal (green)');
+    let coronalChanged = coronalFolder.add(
         coronalRenderer.stackHelper,
         'index', 0, coronalRenderer.stackHelper.orientationMaxIndex).step(1).listen();
-    stackFolder3.add(
+    coronalFolder.add(
         coronalRenderer.stackHelper.slice, 'interpolation', 0, 1).step(1).listen();
     return {redChanged: axialChanged, yellowChanged: sagittalChanged, greenChanged: coronalChanged};
 }
